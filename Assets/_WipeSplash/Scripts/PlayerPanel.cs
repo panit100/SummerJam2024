0094771b0d6e911c078e49f76831ac51e69f3e37
Wai <wai713952@gmail.com>
2024-05-22 22:43:38 +0700

    ADD: heal, re-stamina, block sound effect

--- Assets/_WipeSplash/Scripts/PlayerPanel.cs
+++ Assets/_WipeSplash/Scripts/PlayerPanel.cs
@@ -148,6 +148,9 @@ public class PlayerPanel : MonoBehaviour
         }
         if (currentBlock > 0)
         {
+            // TODO: add block effect
+            SoundManager.Instance.PlaySFX("SFX_Effect_Block");
+
             currentBlock -= damage;
             if (currentBlock < 0)
             {
@@ -206,6 +209,9 @@ public class PlayerPanel : MonoBehaviour
 
     void OnRegenStamina(float regenStamina)
     {
+        // TODO: increase stamina effect
+        SoundManager.Instance.PlaySFX("SFX_Effect_Stamina");
+
         currentStamina += regenStamina;
 
         if (currentStamina >= stamina)
@@ -216,6 +222,9 @@ public class PlayerPanel : MonoBehaviour
 
     void OnRegenHp(float regenHp)
     {
+        // TODO: regen hp effect
+        SoundManager.Instance.PlaySFX("SFX_Effect_Heal");
+
         currentHp += regenHp;
         if (currentHp >= hp)
             currentHp = hp;
