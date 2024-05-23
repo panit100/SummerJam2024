0a2fe6b47a1b3ffcb618f2b0456dc4158d8ad868
Wai <wai713952@gmail.com>
2024-05-23 10:50:18 +0700

    ADD: heal and regen stamina particle, REMOVE: heal and regen stamina image tween

--- Assets/_WipeSplash/Scripts/ItemEffect.cs
+++ Assets/_WipeSplash/Scripts/ItemEffect.cs
@@ -8,46 +8,32 @@ public class ItemEffect : MonoBehaviour
 {
 
     [Header("Image")]
-    [SerializeField] private Image healImage;
-    [SerializeField] private Image staminaImage;
+    [SerializeField] private ParticleSystem healParticle;
+    [SerializeField] private ParticleSystem staminaParticle;
     [SerializeField] private Image blockImage;
 
     [Header("Effect Position")]
-    [SerializeField] private RectTransform startPosition;
+    [SerializeField] private RectTransform startTransform;
 
     void Start()
     {
-        healImage.color = new Color();
-        staminaImage.color = new Color();
         blockImage.rectTransform.localScale = new Vector3();
     }
 
     public void PlayHealEffect()
     {
         SoundManager.Instance.PlaySFX("SFX_Effect_Heal");
-
-        healImage.color = new Color(1, 1, 1, 1);
-        healImage?.DOKill();
-
-        healImage.rectTransform.position = startPosition.position + new Vector3(Random.Range(1.5f, -1.5f), 0, 0);
-        healImage.rectTransform.DOLocalMoveY(50, .25f);
-        healImage.DOFade(0, .25f);
+        healParticle.Play();
     }
     public void PlayStaminaEffect()
     {
         SoundManager.Instance.PlaySFX("SFX_Effect_Stamina");
-
-        staminaImage.color = new Color(1, 1, 1, 1);
-        staminaImage?.DOKill();
-
-        staminaImage.rectTransform.position = startPosition.position + new Vector3(Random.Range(1.5f, -1.5f), 0, 0);
-        staminaImage.rectTransform.DOLocalMoveY(50, .25f);
-        staminaImage.DOFade(0, .25f);
+        staminaParticle.Play();
     }
     public void PlayBlockEffect()
     {
         blockImage?.DOKill();
-        blockImage.rectTransform.DOScale(1.5f, 0.2f);
-        blockImage.rectTransform.DOScale(0, 0.8f);
+        blockImage.rectTransform.DOScale(1.5f, 0.1f)
+        .OnComplete(() => blockImage.rectTransform.DOScale(0, 0.35f));
     }
 }
